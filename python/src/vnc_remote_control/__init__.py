b178fb81753c29719b20aa68914097a042b0fcf8
Phillip Chin <ekkus93@gmail.com>
2026-08-07 02:35:21 -0700

    Export Python client public API

---
+++ python/src/vnc_remote_control/__init__.py
@@ -0,0 +1,42 @@
+from .client import VncClient, VncRemoteControlClient
+from .errors import (
+    ApiError,
+    OptionalDependencyError,
+    ProtocolError,
+    TransportError,
+    VncRemoteControlError,
+)
+from .models import (
+    ClipboardResponse,
+    CommandAcceptedResponse,
+    ConnectionState,
+    DisplayResponse,
+    Event,
+    HealthResponse,
+    KeyAction,
+    MouseButton,
+    ScreenshotResponse,
+    StatusResponse,
+    WorkerFailure,
+)
+
+__all__ = [
+    "ApiError",
+    "ClipboardResponse",
+    "CommandAcceptedResponse",
+    "ConnectionState",
+    "DisplayResponse",
+    "Event",
+    "HealthResponse",
+    "KeyAction",
+    "MouseButton",
+    "OptionalDependencyError",
+    "ProtocolError",
+    "ScreenshotResponse",
+    "StatusResponse",
+    "TransportError",
+    "VncClient",
+    "VncRemoteControlClient",
+    "VncRemoteControlError",
+    "WorkerFailure",
+]
